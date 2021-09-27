db7e1c449695034556a96b5872ef6fbe90b33f09
Cyril <cyrildeveloppeur@gmail.com>
2021-09-27 21:22:37 +0200

    Delete import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import "../styles/normalize.css"; // Next.js authorizes Materialize.css (Global CSS) to be imported ONLY from _app.js
-import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 import Layout from "../Components/Layout";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Layout>
-      <Component {...pageProps} />
+    <Component {...pageProps} />
   </Layout>
 );
 
